9ff596a02e9c9a03fb6b1335258836d9d0177a5f
Ger <germancandradem@gmail.com>
2025-02-19 05:19:26 -0500

    Added material and venom added build config true added notification permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
@@ -49,6 +50,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.venom)
+    implementation(libs.material)
+//    implementation(libs.vemom.debug)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/ger/gradleplayground/MainActivity.kt
+++ app/src/main/java/com/ger/gradleplayground/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.ger.gradleplayground
 
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import com.ger.gradleplayground.ui.theme.GradlePlaygroundTheme
+import com.github.venom.Venom
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var venom: Venom
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= 33) {
+            notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        } else {
+            hasNotificationPermissionGranted = true
+        }
+        venom = Venom.createInstance(this)
+        venom.initialize()
         enableEdgeToEdge()
         setContent {
             GradlePlaygroundTheme {
@@ -26,4 +41,45 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private val notificationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            hasNotificationPermissionGranted = isGranted
+            if (!isGranted) {
+                if (Build.VERSION.SDK_INT >= 33) {
+                    if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                        showNotificationPermissionRationale()
+                    }
+                }
+            } else {
+                venom.start()
+                Toast.makeText(
+                    applicationContext,
+                    "notification permission granted",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+        }
+
+
+    private fun showNotificationPermissionRationale() {
+
+        MaterialAlertDialogBuilder(
+            this,
+            com.google.android.material.R.style.MaterialAlertDialog_Material3
+        )
+            .setTitle("Alert")
+            .setMessage("Notification permission is required, to show notification")
+            .setPositiveButton("Ok") { _, _ ->
+                if (Build.VERSION.SDK_INT >= 33) {
+                    notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+    var hasNotificationPermissionGranted = false
+
 }
